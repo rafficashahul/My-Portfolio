bdc5d6c491960c49b990f906d16058801426be3c
rafficashahul <raffica26sandefjord@gmail.com>
2026-06-15 08:18:12 +0200

    fix: fix mobile nav overflow with hamburger menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,41 +1,53 @@
-import styles from "./Header.module.css";
-import { Link } from "react-scroll";
+import { useState } from 'react'
+import { Link } from 'react-scroll'
+import styles from './Header.module.css'
+
+const navItems = ['about', 'skills', 'projects', 'experience', 'contact']
 
 const Header = () => {
-  return (
-    <div className={styles.header}>
-      <nav className={styles.nav}>
-        <h1 className={styles.logo}>Developer</h1>
-        <ul className={styles.menu}>
-          <li>
-            <Link to="about" smooth={true} duration={500}>
-              About
-            </Link>
-          </li>
-          <li>
-            <Link to="skills" smooth={true} duration={500}>
-              Skills
-            </Link>
-          </li>
-          <li>
-            <Link to="projects" smooth={true} duration={500}>
-              Projects
-            </Link>
-          </li>
-          <li>
-            <Link to="experience" smooth={true} duration={500}>
-              Experience
-            </Link>
-          </li>
-          <li>
-            <Link to="contact" smooth={true} duration={500}>
-              Contact
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    </div>
-  );
-};
+  const [isOpen, setIsOpen] = useState(false)
 
-export default Header;
+  const toggleMenu = () => setIsOpen(!isOpen)
+  const closeMenu = () => setIsOpen(false)
+
+  return (
+    <header className={styles.header}>
+      <nav className={styles.nav}>
+
+        {/* Logo */}
+        <div className={styles.logo}>Developer</div>
+
+        {/* Hamburger Button */}
+        <button
+          className={styles.hamburger}
+          onClick={toggleMenu}
+          aria-label="Toggle menu"
+        >
+          <span className={`${styles.bar} ${isOpen ? styles.bar1Open : ''}`} />
+          <span className={`${styles.bar} ${isOpen ? styles.bar2Open : ''}`} />
+          <span className={`${styles.bar} ${isOpen ? styles.bar3Open : ''}`} />
+        </button>
+
+        {/* Menu */}
+        <ul className={`${styles.menu} ${isOpen ? styles.menuOpen : ''}`}>
+          {navItems.map((item) => (
+            <li key={item}>
+              <Link
+                to={item}
+                smooth={true}
+                duration={500}
+                className={styles.link}
+                onClick={closeMenu}
+              >
+                {item}
+              </Link>
+            </li>
+          ))}
+        </ul>
+
+      </nav>
+    </header>
+  )
+}
+
+export default Header
